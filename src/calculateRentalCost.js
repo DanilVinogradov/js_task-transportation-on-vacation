d1d8bf2a17a813ab6c9f772f63c0d7683302de62
Arramis <danilvino228@gmail.com>
2025-11-03 13:18:56 +0200

    Calculate

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,26 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const oneDayRent = 40;
+  const sevenDaysDiscount = 50;
+  const threeDaysDiscount = 20;
+  let sumRent = 0;
+
+  if (days >= 7) {
+    sumRent = days * oneDayRent - sevenDaysDiscount;
+
+    return sumRent;
+  }
+
+  if (days >= 3) {
+    sumRent = days * oneDayRent - threeDaysDiscount;
+
+    return sumRent;
+  } else {
+    sumRent = days * oneDayRent;
+
+    return sumRent;
+  }
 }
 
 module.exports = calculateRentalCost;
